c0d7219c9b8d83d0bc24401281998ca43ccf2a46
vincentLiuxiang <hitliuyaguang@foxmail.com>
2016-11-29 13:09:34 +0800

    Error handling

--- static.go
+++ static.go
@@ -7,69 +7,64 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-var DefaultFS = &FS{
-	&fasthttp.FS{
-		Compress:        true,
-		IndexNames:      []string{"index.html"},
-		Root:            ".",
-		AcceptByteRange: true,
-	},
-	true,
+var DefaultFS = &fasthttp.FS{
+	Compress:        true,
+	IndexNames:      []string{"index.html"},
+	Root:            ".",
+	AcceptByteRange: true,
 }
 
-type FS struct {
-	// Fasthttp *fasthttp.FS
-	*fasthttp.FS
-	// When something error occours,
-	// FallThrough == true, next(nil)
-	// FallThrough == false, next(err)
-	FallThrough bool
-}
-
-func New(fs *FS) func(ctx *fasthttp.RequestCtx, next func(error)) {
+// fs is a value which references to a *fasthttp.FS pointer.
+// The reason why New accepts a fasthttp.FS value but not a *fasthttp.FS pointer
+// is that after we called New() and return a lu Middleware, we don't
+// hope anyone can change any property of fs(fasthttp.FS).
+// However, if New accepts a *fasthttp.FS pointer, we can change properties
+// of fasthttp.FS even after we called New().
+//
+// if the http request method is not 'GET' or 'HEAD', the static will call next(nil)
+//
+// if the http request file does not exist or the directory does not have the IndexNames file.
+// the static will call next(nil)
+//
+// or, fasthttp.FS will handle the request file or the IndexNames file in the request directory
+func New(fs fasthttp.FS) func(ctx *fasthttp.RequestCtx, next func(error)) {
 	staticHandler := fs.NewRequestHandler()
 	return func(ctx *fasthttp.RequestCtx, next func(error)) {
 		m := string(ctx.Method())
 		if m != "GET" && m != "HEAD" {
-			if fs.FallThrough {
-				next(nil)
-				return
-			}
-			next(errors.New("405 Method Not Allowed"))
-			// // method not allowed
-			// ctx.SetStatusCode(405)
-			// ctx.Response.Header.Set("Allow", "GET,HEAD")
-			// ctx.Response.Header.Set("Content-Length", "0")
+			next(nil)
 			return
 		}
 
-		fileInfo, err := os.Stat(fs.Root + string(ctx.Path()))
+		path := string(ctx.Path())
+
+		fileInfo, err := os.Stat(fs.Root + path)
 		// if err != nil && os.IsNotExist(err) {
 		if err != nil {
-			if fs.FallThrough {
-				next(nil)
-				return
-			}
-			next(err)
+			next(nil)
 			return
 		}
 
+		// An exist file
+		// fasthttp.FS handle it
 		if !fileInfo.IsDir() {
 			staticHandler(ctx)
 			return
 		}
 
-		errPath := filterPath(fs.Root+string(ctx.Path())+"/", fs.IndexNames, next)
-		if errPath != nil {
-			if fs.FallThrough {
-				next(nil)
-				return
-			}
-			next(errPath)
+		if len(fs.IndexNames) == 0 {
+			next(nil)
 			return
 		}
-		staticHandler(ctx)
 
+		errPath := filterPath(fs.Root+path+"/", fs.IndexNames, next)
+		if errPath != nil {
+			next(nil)
+			return
+		}
+
+		// An exist directory has IndexNames file
+		staticHandler(ctx)
 	}
 }
 
